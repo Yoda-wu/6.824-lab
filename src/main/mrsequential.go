d6050c3d6f007f9399833eb06c38e0bb0fb1acf5
Marchons <d1286360646@outlook.com>
2022-11-11 23:20:16 +0800

    feat: 定义worker和master之间的交互方式(unfinish)

--- src/main/mrsequential.go
+++ src/main/mrsequential.go
@@ -86,10 +86,8 @@ func main() {
 	ofile.Close()
 }
 
-//
 // load the application Map and Reduce functions
 // from a plugin file, e.g. ../mrapps/wc.so
-//
 func loadPlugin(filename string) (func(string, string) []mr.KeyValue, func(string, []string) string) {
 	p, err := plugin.Open(filename)
 	if err != nil {
