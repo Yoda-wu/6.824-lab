43192e9a01441ac817e114bd531598fe3ef85ada
Marchons <d1286360646@outlook.com>
2022-11-24 22:58:02 +0800

    feat: lab2B, pass some test but not all

--- src/main/test-main.go
+++ src/main/test-main.go
@@ -4,15 +4,8 @@ import "fmt"
 
 func main() {
 
-	for y := 1.5; y > -1.5; y -= 0.1 {
-		for x := -1.5; x < 1.5; x += 0.05 {
-			a := x*x + y*y - 1
-			if a*a*a-x*x*y*y*y <= 0.0 {
-				fmt.Print("*")
-			} else {
-				fmt.Print(" ")
-			}
-		}
-		fmt.Print("\n")
-	}
+	var test_slice []int = []int{1, 2, 3, 4}
+	//
+	//fmt.Println(test_slice[:])
+	fmt.Println(test_slice[:4])
 }
